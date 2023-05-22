e382fc43b7cc8008207cce5b62b464e60d5f9496
Mike <reudymiguel20@gmail.com>
2023-05-21 20:30:37 -0400

    Stage 2/8: 2+2+

--- Main.java
+++ Main.java
@@ -6,10 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        System.out.println(num1 + num2);
+
+        while (true) {
+//            int num1 = Integer.parseInt(scanner.nextLine());
+//            int num2 = Integer.parseInt(scanner.nextLine());
+            String numbers = scanner.nextLine();
+            if (numbers.equals("/exit")) {
+                System.out.println("Bye!");
+                break;
+            }
+
+            String[] numbersArray = numbers.split(" ");
+            if (numbers.equals("")) {
+                continue;
+            } else if (numbersArray.length == 1) {
+                System.out.println(numbers);
+            } else {
+                int num1 = Integer.parseInt(numbersArray[0]);
+                int num2 = Integer.parseInt(numbersArray[1]);
+                System.out.println(num1 + num2);
+            }
+
+        }
+
 
     }
 }
